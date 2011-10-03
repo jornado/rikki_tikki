87832808e3a505e8f3a70727af3bba0df38020f1
poeks <poeksweb@gmail.com>
2011-10-03 09:44:27 -0700

    Env tweaks

--- config/env.rb
+++ config/env.rb
@@ -6,13 +6,12 @@ Bundler.require :default
 TIMEZONE = 'US/Pacific'
 ENV['TZ'] = TIMEZONE
 
-root = '.'
-confit File.join(root, 'config', 'app.yml'), ENV['RACK_ENV']
-Dir.glob(File.join(root, 'lib', '**/*.rb')).each { |f| require f }
-Dir.glob(File.join(root, 'models', '**/*.rb')).each { |f| require f }
+confit File.join('.', 'config', 'app.yml'), ENV['RACK_ENV']
+Dir.glob(File.join('.', 'lib', '**/*.rb')).each { |f| require f }
+Dir.glob(File.join('.', 'models', '**/*.rb')).each { |f| require f }
 
 DataMapper.finalize
 DataMapper::Logger.new(STDOUT, :debug) if confit.app.debug
-DataMapper.setup :default, YAML.load_file(File.join(root, 'config', 'database.yml'))[ENV['RACK_ENV']]
+DataMapper.setup :default, YAML.load_file(File.join('.', 'config', 'database.yml'))[ENV['RACK_ENV']]
         
 puts "Rikki Tikki started at #{Time.now}"
